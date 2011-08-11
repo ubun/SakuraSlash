9c14ea321ee549ec3d75fb85186149b2d04b187e
ubun <ubun@foxmail.com>
2011-08-12 01:49:01 +0800

    Updata Files.

---
+++ src/choosegeneraldialog.h
@@ -0,0 +1,50 @@
+#ifndef CHOOSEGENERALDIALOG_H
+#define CHOOSEGENERALDIALOG_H
+
+class General;
+
+#include <QDialog>
+#include <QProgressBar>
+#include <QGroupBox>
+#include <QButtonGroup>
+
+class ChooseGeneralDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ChooseGeneralDialog(const QStringList &general_names,
+                                 QWidget *parent);
+
+protected:
+    virtual void timerEvent(QTimerEvent *);
+
+private:
+    QProgressBar *progress_bar;
+    QDialog *free_chooser;
+
+private slots:
+    void freeChoose();
+};
+
+class FreeChooseDialog: public QDialog{
+    Q_OBJECT
+
+public:
+    explicit FreeChooseDialog(QWidget *parent, bool pair_choose = false);
+
+private:
+    QButtonGroup *group;
+    bool pair_choose;
+    QGroupBox *createGroupBox(const QList<const General *> &generals);
+
+private slots:
+    void chooseGeneral();
+    void uncheckExtraButton(QAbstractButton *button);
+
+signals:
+    void general_chosen(const QString &name);
+    void pair_chosen(const QString &first, const QString &second);
+};
+
+#endif // CHOOSEGENERALDIALOG_H
