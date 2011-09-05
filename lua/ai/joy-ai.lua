c8f51514e5ba214029c3eefb6b96cf644c1dbb5b
ubun <ubun@foxmail.com>
2011-09-05 22:38:21 +0800

    Clear all.

--- lua/ai/joy-ai.lua
+++
@@ -1,5 +0,0 @@
--- when enemy using the peach
-sgs.ai_skill_invoke["grab_peach"] = function(self, data)
-	local struct= data:toCardUse()
-	return self:isEnemy(struct.from)
-end
--- swig/swig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-swig -c++ -lua sanguosha.i
