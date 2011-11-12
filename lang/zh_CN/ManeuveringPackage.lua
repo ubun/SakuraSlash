9dec36592aa153edba307aa03f64eed8548be816
Edogawa <ubun@foxmail.com>
2011-11-12 17:12:58 +0800

    マップを増加させる

--- lang/zh_CN/ManeuveringPackage.lua
+++ lang/zh_CN/ManeuveringPackage.lua
@@ -1,7 +1,7 @@
 -- translation for ManeuveringPackage
 
 return {
-	["maneuvering"] = "军争", 
+	["maneuvering"] = "增补卡牌包", 
 	["buff_card"] = "辅助伤害卡", 
 	["damage_spread"] = "伤害传导", 
 	["fire_attack"] = "火攻", 
