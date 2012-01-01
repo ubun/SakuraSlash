9db44aec55f85708a9021d1b343b85c202de8a85
Ubun Tenkei <ubun@foxmail.com>
2012-01-01 12:29:43 +0800

    虽然没改什么，但也要与时俱进……

--- lang/zh_CN/ChallengeModeSet.lua
+++ lang/zh_CN/ChallengeModeSet.lua
@@ -5,7 +5,7 @@ return {
 	["@disaster4fr"] = "腐女的灾难", 
 	["@p4luoyi"] = "裸衣的横扫", 
 	["@test4pile"] = "牌堆的考验", 
-	["@mythology"] = "神的传说", 
+	["@mythology"] = "神的传说",
 	["@2strike"] = "二刀的猛进", 
 	["@tortoise"] = "万年打不动的老乌龟", 
 	["@manshow"] = "抓狂的闷骚", 
