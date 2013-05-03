c43e610df9c744f1b28d0f21ea4a16d4b485e142
Ubun <ubun@foxmail.com>
2013-05-03 19:49:11 +0800

    老鼠肉加明胶变身羊肉
    无锡制售假羊肉案，嫌犯购入狐狸、老鼠等未经检验检疫的动物肉制品，添加明胶、胭脂红、硝盐等冒充羊肉销售至苏、沪等地，案值1000余万元。毁三观啊！老鼠肉+加明胶+胭脂红+硝盐=羊肉。这样的“羊肉串”你还敢吃吗？
    【给我一个放心吃的理由】
    各地死猪事件，排骨汤喝了个够；H7N9的出现，对家禽望而生畏；如今十二生肖之首也站了起来，不敢想象：老鼠肉+明胶+胭脂红+硝盐=羊肉。那些年，我们嗜杀成性，太多的鼠儿命丧我口，抬头45度角仰望天空，下一个会是谁？

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -1002,70 +1002,6 @@ local function getSkillViewCard(card, class_name, player, card_place)
  	end
 end
 
-function SmartAI:searchForAnaleptic(use,enemy,slash)
-	if not self.toUse then return nil end
-
-	for _,card in ipairs(self.toUse) do
-		if card:getId()~= slash:getId() then return nil end
-	end
-
-	if not use.to then return nil end
-	if self.player:hasUsed("Analeptic") then return nil end
-
-	local cards = self.player:getHandcards()
-	cards = sgs.QList2Table(cards)
-	self:fillSkillCards(cards)
-
-	if (getDefense(self.player) < getDefense(enemy)) and
-		(self.player:getHandcardNum() < 1+self.player:getHp()) or
-		self.player:hasFlag("drank") then
-			return
-	end
-
-	if enemy:getArmor() then
-		if ((enemy:getArmor():objectName()) == "eight_diagram")
-			or ((enemy:getArmor():objectName()) == "silver_lion") then
-			if (self.player:getHandcardNum() <= 1+self.player:getHp()) then
-				return
-			end
-		end
-	end
-
-	if self.player:getPhase() == sgs.Player_Play then
-		if self.player:hasFlag("lexue") then
-			local lexuesrc = sgs.Sanguosha:getCard(self.player:getMark("lexue"))
-			if lexuesrc:inherits("Analeptic") then
-				local cards = sgs.QList2Table(self.player:getHandcards())
-				self:sortByUseValue(cards, true)
-				for _, hcard in ipairs(cards) do
-					if hcard:getSuit() == lexuesrc:getSuit() then
-						local lexue = sgs.Sanguosha:cloneCard("analeptic", lexuesrc:getSuit(), lexuesrc:getNumber())
-						lexue:addSubcard(hcard:getId())
-						lexue:setSkillName("lexue")
-						if self:getUseValue(lexuesrc) > self:getUseValue(hcard) then
-							return lexue
-						end
-					end
-				end
-			end
-		end
-
-		if self.player:hasLordSkill("weidai") and not self.player:hasUsed("WeidaiCard") then
-			return sgs.Card_Parse("@WeidaiCard=.")
-		end
-	end
-
-	local card_str = self:getCardId("Analeptic")
-	if card_str then return sgs.Card_Parse(card_str) end
-
-	for _, anal in ipairs(cards) do
-		if (anal:className() == "Analeptic") and not (anal:getEffectiveId() == slash:getEffectiveId()) and
-			not isCompulsoryView(anal, "Slash", self.player, sgs.Player_Hand) then
-			return anal
-		end
-	end
-end
-
 function SmartAI:slashProhibit(card,enemy)
 	if card == nil then
 		card = sgs.Sanguosha:cloneCard("slash", sgs.Card_NoSuit, 0)
@@ -1532,64 +1468,6 @@ function SmartAI:useCardSnatch(snatch, use)
 	end
 end
 
-function SmartAI:useCardFireAttack(fire_attack, use)
-	if self.player:hasSkill("wuyan") or self.player:hasSkill("tianzhen") then return end
-	local lack = {
-		spade = true,
-		club = true,
-		heart = true,
-		diamond = true,
-	}
-
-	local targets_succ = {}
-	local targets_fail = {}
-	local cards = self.player:getHandcards()
-	for _, card in sgs.qlist(cards) do
-		if card:getEffectiveId() ~= fire_attack:getEffectiveId() then
-			lack[card:getSuitString()] = false
-		end
-	end
-
-	if self.player:hasSkill("hongyan") then
-		lack["spade"] = true
-	end
-
-	self:sort(self.enemies, "defense")
-	for _, enemy in ipairs(self.enemies) do
-		if (self:objectiveLevel(enemy) > 3) and not enemy:isKongcheng() and self:hasTrickEffective(fire_attack, enemy) then
-
-			local cards = enemy:getHandcards()
-			local success = true
-			for _, card in sgs.qlist(cards) do
-				if lack[card:getSuitString()] then
-					success = false
-					break
-				end
-			end
-
-			if success then
-				if self:isEquip("Vine", enemy) then
-					table.insert(targets_succ, 1, enemy)
-					break
-				else
-					table.insert(targets_succ, enemy)
-				end
-			else
-				table.insert(targets_fail, enemy)
-			end
-		end
-	end
-
-	if #targets_succ > 0 then
-		use.card = fire_attack
-		if use.to then use.to:append(targets_succ[1]) end
-	elseif #targets_fail > 0 and self:getOverflow(self.player) > 0 then
-		use.card = fire_attack
-		local r = math.random(1, #targets_fail)
-		if use.to then use.to:append(targets_fail[r]) end
-	end
-end
-
 function SmartAI:useCardByClassName(card, use)
 	local class_name = card:className()
 	local use_func = self["useCard" .. class_name]
@@ -1635,26 +1513,6 @@ function SmartAI:useCardDuel(duel, use)
 					return
 				end
 			end
-		end
-	end
-end
-
-local function handcard_subtract_hp(a, b)
-	local diff1 = a:getHandcardNum() - a:getHp()
-	local diff2 = b:getHandcardNum() - b:getHp()
-
-	return diff1 < diff2
-end
-
-function SmartAI:useCardSupplyShortage(card, use)
-	table.sort(self.enemies, handcard_subtract_hp)
-
-	local enemies = self:exclude(self.enemies, card)
-	for _, enemy in ipairs(enemies) do
-		if ((#enemies == 1) or not enemy:hasSkill("tiandu")) and not enemy:containsTrick("supply_shortage") then
-			use.card = card
-			if use.to then use.to:append(enemy) end
-			return
 		end
 	end
 end
@@ -1726,31 +1584,6 @@ function SmartAI:useCardCollateral(card, use)
 
 		end
 		n = nil
-	end
-end
-
-function SmartAI:useCardIronChain(card, use)
-	local targets = {}
-	self:sort(self.friends,"defense")
-	for _, friend in ipairs(self.friends) do
-		if friend:isChained() then
-			table.insert(targets, friend)
-		end
-	end
-
-	self:sort(self.enemies,"defense")
-	for _, enemy in ipairs(self.enemies) do
-		if not enemy:isChained() and not self.room:isProhibited(self.player, enemy, card) and not enemy:hasSkill("danlao")
-			and self:hasTrickEffective(card, enemy) and not (self:objectiveLevel(enemy) <= 3) then
-			table.insert(targets, enemy)
-		end
-	end
-
-	use.card = card
-
-	if targets[2] and not self.player:hasSkill("wuyan") then
-		if use.to then use.to:append(targets[1]) end
-		if use.to then use.to:append(targets[2]) end
 	end
 end
 
@@ -3053,26 +2886,6 @@ function SmartAI:askForSinglePeach(dying)
 	return card_str or "."
 end
 
-function SmartAI:getChainedFriends()
-	local chainedFriends = {}
-	for _, friend in ipairs(self.friends) do
-		if friend:isChained() then
-			table.insert(chainedFriends,friend)
-		end
-	end
-	return chainedFriends
-end
-
-function SmartAI:getChainedEnemies()
-	local chainedEnemies = {}
-	for _, enemy in ipairs(self.enemies) do
-		if enemy:isChained() then
-			table.insert(chainedEnemies,enemy)
-		end
-	end
-	return chainedEnemies
-end
-
 function SmartAI:hasSkill(skill)
 	local skill_name = skill
 	if type(skill) == "table" then
@@ -3632,6 +3445,7 @@ dofile "lua/ai/general_config.lua"
 dofile "lua/ai/value_config.lua"
 dofile "lua/ai/standard-ai.lua"
 dofile "lua/ai/standard-skill-ai.lua"
+dofile "lua/ai/black_dragon-ai.lua"
 dofile "lua/ai/thunder_bird-ai.lua"
 dofile "lua/ai/chat-ai.lua"
 -- dofile "lua/ai/hulaoguan-ai.lua"
